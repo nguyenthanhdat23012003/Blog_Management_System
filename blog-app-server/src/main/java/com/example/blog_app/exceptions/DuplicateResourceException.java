1b8639a2324a23aebf11173a5568a459dff039d8
Nguyễn Thành Đạt <77061686+nguyenthanhdat23012003@users.noreply.github.com>
2024-11-13 19:23:01 +0700

    Revert "Server role"

--- blog-app-server/src/main/java/com/example/blog_app/exceptions/DuplicateResourceException.java
+++
@@ -1,7 +0,0 @@
-package com.example.blog_app.exceptions;
-
-public class DuplicateResourceException extends RuntimeException {
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
--- blog-app-server/src/main/java/com/example/blog_app/exceptions/ImmutableResourceException.java
+++
@@ -1,7 +0,0 @@
-package com.example.blog_app.exceptions;
-
-public class ImmutableResourceException extends RuntimeException {
-    public ImmutableResourceException(String message) {
-        super(message);
-    }
-}
