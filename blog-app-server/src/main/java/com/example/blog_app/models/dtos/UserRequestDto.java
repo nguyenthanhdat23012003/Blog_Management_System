1199f9c5b0c1814405dcc8dd1f39572920eeb012
nguyenthanhdat23012003 <nguyenthanhdat23012003@gmail.com>
2024-11-18 17:07:24 +0700

    Server-blog: Update and Create user logic separation update

--- blog-app-server/src/main/java/com/example/blog_app/models/dtos/UserRequestDto.java
+++ blog-app-server/src/main/java/com/example/blog_app/models/dtos/UserRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.blog_app.models.dtos;
 
+import com.example.blog_app.common.validation.CreateValidationGroup;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
@@ -13,15 +14,15 @@ import lombok.Setter;
 public class UserRequestDto {
     private int id;
 
-    @NotEmpty(message = "Name cannot be empty")
+    @NotEmpty(message = "Name cannot be empty", groups = CreateValidationGroup.class)
     @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
     private String name;
 
-    @NotEmpty(message = "Email cannot be empty")
+    @NotEmpty(message = "Email cannot be empty", groups = CreateValidationGroup.class)
     @Email(message = "Email should be valid")
     private String email;
 
-    @NotEmpty(message = "Password cannot be empty")
+    @NotEmpty(message = "Password cannot be empty", groups = CreateValidationGroup.class)
     @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
 
